351d9085ca5acd72ab2c78e220f85f7bc8f55ace
Frederik Rouseré <frederik@sitesource.be>
2023-10-29 19:07:54 +0100

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,8 +6,8 @@ export default function Home() {
   return (
     <main>
       <Title>Strategea</Title>
-      <img src="strategea.svg"></img><br/>
-      <a href="mailto:info@strategea.be">contact</a>
+      <img src="strategea.svg"></img>
+      <p><a href="#" id="style-2" data-replace="contact"><span>contact</span></a> me</p>
     </main>
   );
 }
